31499c6bcad228a403c6b634744081cffe4c3066
Yongil Choi <hurstchoi@daum.net>
2020-01-15 15:02:01 +0900

    log.Info(...genesis.go...), if you do not integrate this, just log.info,

--- common/qManHelper.go
+++ common/qManHelper.go
@@ -1,9 +1,9 @@
 package common
 
 import (
+	"fmt"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/syndtr/goleveldb/leveldb"
-	"net"
 )
 
 
@@ -11,12 +11,6 @@ var (
 
 	QManagerStorage *leveldb.DB
 	QManConnected bool
-	QManagerAddress *net.UDPAddr
-	BootNodeAddress *net.UDPAddr
-	QManagerNodeIDStr string
-	QManReady bool
-	BootNodeReady bool
-	BootNodeQManAddr string
 )
 //
 //type (
@@ -44,37 +38,27 @@ func Find(nodeId string) ( found bool) {
 	//QManagerStorage, err = leveldb.OpenFile("level", nil)
 
 	//var data []byte
-	if QManConnected {
-		_, err := QManagerStorage.Get([]byte(nodeId), nil)
-		log.Info("Node Not Found", "err = ", err)
-		if err != nil {
-			return false
-		}
-		//fmt.Println(data)
-
-		//dec := gob.NewDecoder(bytes.NewReader(data))
-		//
-		//dec.Decode(node)
-
-		//defer QManagerStorage.Close()
-		return true
-
+	data, err := QManagerStorage.Get([]byte(nodeId), nil)
+	if err != nil {
+		return false
 	}
+	fmt.Println(data)
 
+	//dec := gob.NewDecoder(bytes.NewReader(data))
+	//
+	//dec.Decode(node)
+
+	//defer QManagerStorage.Close()
 	return true
-
 }
 
 
 func Save(ID []byte, address []byte) ( saved bool) {
-	if QManConnected {
-		err := QManagerStorage.Put(ID, address, nil)
 
-		if err != nil {
-			return false
-		}
-		return true
+	err := QManagerStorage.Put(ID, address, nil)
 
+	if err != nil{
+		return false
 	}
 	return true
 }
