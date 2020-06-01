2f3c80db7278a9c9f99a306c8102a03a37eafd47
Yongil Choi <hurstchoi@daum.net>
2020-06-01 16:24:49 +0900

    test

--- consensus/podc/core/roundchange.go
+++ consensus/podc/core/roundchange.go
@@ -87,44 +87,44 @@ func (c *core) sendRoundChange(round *big.Int) {
 
 func (c *core) handleRoundChange(msg *message, src podc.Validator) error {
 	logger := c.logger.New("state", c.state)
+	if (!reflect.DeepEqual(c.qmanager, c.Address())) { //if I'm Qmanager
 
-
-	// Decode round change message
-	var rc *roundChange
-	var number int
-	if err := msg.Decode(&rc); err != nil {
-		logger.Error("Failed to decode round change", "err", err)
-		return errInvalidMessage
-	}
-
-	cv := c.currentView()
-
-	// We never accept round change message with different sequence number
-	if rc.Sequence.Cmp(cv.Sequence) != 0 {
-		logger.Warn("Inconsistent sequence number", "expected", cv.Sequence, "got", rc.Sequence)
-		return errInvalidMessage
-	}
-
-	// We never accept round change message with smaller round number
-	if rc.Round.Cmp(cv.Round) < 0 {
-		logger.Warn("Old round change", "from", src, "expected", cv.Round, "got", rc.Round)
-		return errOldMessage
-	}
-
-	// Add the round change message to its message set and return how many
-	// messages we've got with the same round number and sequence number.
-	if (reflect.DeepEqual(c.qmanager, c.Address())) { //if I'm Qmanager
-		//log.Info("I'm Qmanager address: ", "c.qmanager", c.qmanager, "Self address", c.Address())
-		//if( qManager.QManConnected ){
-		log.Info("I'm Qmanager handleRoundChange ", "cv.Sequence", cv.Sequence, "cv.Round", cv.Round)
-		num, err := c.roundChangeSet.Set(rc.Round, msg)
-		number = num
-		if err != nil {
-			logger.Warn("Failed to add round change message", "from", src, "msg", msg, "err", err)
-			return err
+		// Decode round change message
+		var rc *roundChange
+		var number int
+		if err := msg.Decode(&rc); err != nil {
+			logger.Error("Failed to decode round change", "err", err)
+			return errInvalidMessage
 		}
 
-	} else {
+		cv := c.currentView()
+
+		// We never accept round change message with different sequence number
+		if rc.Sequence.Cmp(cv.Sequence) != 0 {
+			logger.Warn("Inconsistent sequence number", "expected", cv.Sequence, "got", rc.Sequence)
+			return errInvalidMessage
+		}
+
+		// We never accept round change message with smaller round number
+		if rc.Round.Cmp(cv.Round) < 0 {
+			logger.Warn("Old round change", "from", src, "expected", cv.Round, "got", rc.Round)
+			return errOldMessage
+		}
+
+		// Add the round change message to its message set and return how many
+		// messages we've got with the same round number and sequence number.
+
+		//log.Info("I'm Qmanager address: ", "c.qmanager", c.qmanager, "Self address", c.Address())
+		//if( qManager.QManConnected ){
+		//	log.Info("I'm Qmanager handleRoundChange ", "cv.Sequence", cv.Sequence, "cv.Round", cv.Round)
+		//	num, err := c.roundChangeSet.Set(rc.Round, msg)
+		//	number = num
+		//	if err != nil {
+		//		logger.Warn("Failed to add round change message", "from", src, "msg", msg, "err", err)
+		//		return err
+		//	}
+		//
+		//} else {
 
 		num, err := c.roundChangeSet.Add(rc.Round, msg)
 		number = num
@@ -132,27 +132,25 @@ func (c *core) handleRoundChange(msg *message, src podc.Validator) error {
 			logger.Warn("Failed to add round change message", "from", src, "msg", msg, "err", err)
 			return err
 		}
-	}
 
-	// Once we received f+1 round change messages, those messages form a weak certificate.
-	// If our round number is smaller than the certificate's round number, we would
-	// try to catch up the round number.
-	if c.waitingForRoundChange && number == int(c.valSet.F()+1) {
-		if cv.Round.Cmp(rc.Round) < 0 {
-			c.sendRoundChange(rc.Round)
+		// Once we received f+1 round change messages, those messages form a weak certificate.
+		// If our round number is smaller than the certificate's round number, we would
+		// try to catch up the round number.
+		if c.waitingForRoundChange && number == int(c.valSet.F()+1) {
+			if cv.Round.Cmp(rc.Round) < 0 {
+				c.sendRoundChange(rc.Round)
+			}
 		}
+
+		// We've received 2f+1 round change messages, start a new round immediately.
+		if number == int(2*c.valSet.F()+1) {
+			c.startNewRound(&podc.View{
+				Round:    new(big.Int).Set(rc.Round),
+				Sequence: new(big.Int).Set(rc.Sequence),
+			}, true)
+		}
+
 	}
-
-	// We've received 2f+1 round change messages, start a new round immediately.
-	if number == int(2*c.valSet.F()+1) {
-		c.startNewRound(&podc.View{
-			Round:    new(big.Int).Set(rc.Round),
-			Sequence: new(big.Int).Set(rc.Sequence),
-		}, true)
-	}
-
-
-
 
 	return nil
 }
